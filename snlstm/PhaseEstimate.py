eb739cf5d08466e2404f573f5ec5885bd34a9ff3
thomasvrussell <dearthomasrussell@icloud.com>
2022-01-05 17:14:03 +0800

    Update PhaseEstimate.py

--- snlstm/PhaseEstimate.py
+++ snlstm/PhaseEstimate.py
@@ -30,15 +30,16 @@ class FitSingleSpecPhase:
             return mape
         
         AutoDICT = {}
+        PLBound, PUBound = -15.0, +33.01
         # ** initial grid guess with 2 days resolution, phase in full range [-15.0, +33.0]
-        PGuess1 = np.arange(-15.0, +33.01, 2.0)
+        PGuess1 = np.arange(PLBound, PUBound, 2.0)
         for phase_hypo in PGuess1:
             mape = auto_predict(phase_hypo)
             AutoDICT[phase_hypo] = mape
 
         # ** second grid guess with 0.5 days resolution, phase in narrow range: previous best +/- 4 days
         _pbest = min(AutoDICT, key=AutoDICT.get)
-        PGuess2 = np.arange(max(_pbest-4.0, -15.0), min(_pbest+4.0, +33.0), 0.5)
+        PGuess2 = np.arange(max(_pbest-4.0, PLBound), min(_pbest+4.0, PUBound), 0.5)
         for phase_hypo in PGuess2:
             if phase_hypo not in AutoDICT:
                 mape = auto_predict(phase_hypo)
@@ -46,7 +47,7 @@ class FitSingleSpecPhase:
                 
         # ** third grid guess with 0.1 days resolution, phase in narrow range: previous best +/- 1 days
         _pbest = min(AutoDICT, key=AutoDICT.get)
-        PGuess3 = np.arange(max(_pbest-1.0, -15.0), min(_pbest+1.0, +33.0), 0.1)
+        PGuess3 = np.arange(max(_pbest-1.0, PLBound), min(_pbest+1.0, PUBound), 0.1)
         for phase_hypo in PGuess3:
             if phase_hypo not in AutoDICT:
                 mape = auto_predict(phase_hypo)
@@ -57,7 +58,7 @@ class FitSingleSpecPhase:
         MAPE_HP = np.array([AutoDICT[phase_hypo] for phase_hypo in AutoDICT])
         SORT = np.argsort(PHA_HP)
         PHA_HP, MAPE_HP = PHA_HP[SORT], MAPE_HP[SORT]
-        GPHA_HP = np.arange(-15.0, +33.01, 0.05)
+        GPHA_HP = np.arange(PLBound, PUBound, 0.05)
         GMAPE_HP, eGMAPE_HP = GP_Interpolator(PHA_HP, MAPE_HP, np.nan*np.ones(len(MAPE_HP)), GPHA_HP, NaN_fill=FAKE_MAPE_ERROR)
 
         return PHA_HP, MAPE_HP, GPHA_HP, GMAPE_HP, eGMAPE_HP
